93c86ff0fcfcea12afabbf3c191f13c4c4c6bc73
gurlalsingh1313 <150024664+gurlalsingh1313@users.noreply.github.com>
2024-11-30 20:06:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Spline from '@splinetool/react-spline';
 
 // https://vitejs.dev/config/
 export default defineConfig({
